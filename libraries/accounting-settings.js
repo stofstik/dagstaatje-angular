f0965ba571aecde08f751ea9d88d30b711bdea4a
stofstik <verstegen.daan@gmail.com>
2016-06-12 16:45:33 +0200

    initial commit

---
+++ libraries/accounting-settings.js
@@ -0,0 +1,15 @@
+// Settings object that controls default parameters for library methods:
+accounting.settings = {
+    currency: {
+        symbol : "€",   // default currency symbol is '$'
+        format: "%s %v", // controls output: %s = symbol, %v = value/number (can be object: see below)
+        decimal : ",",  // decimal point separator
+        thousand: ".",  // thousands separator
+        precision : 2   // decimal places
+    },
+    number: {
+        precision : 0,  // default precision on numbers is 0
+        thousand: ".",
+        decimal : ","
+    }
+};
