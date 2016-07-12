f17d3452ecf7545b83431a307c29ce99abbd7d92
stofstik <verstegen.daan@gmail.com>
2016-07-12 23:07:57 +0200

    Added angular material design theming

--- src/client/app.js
+++ src/client/app.js
@@ -1,1 +1,7 @@
-var dagstaatjeApp = angular.module('dagstaatjeApp', ['ngRoute', 'ngAnimate', 'ngStorage', 'ngAria', 'ngMessages', 'ngMaterial']);
+var dagstaatjeApp = angular.module('dagstaatjeApp', ['ngRoute', 'ngAnimate', 'ngStorage', 'ngAria', 'ngMessages', 'ngMaterial'])
+    // Set the material design color theme
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('green')
+            .accentPalette('orange');
+    });
